1756f8a7609a3ded8738fd08868ed476d877bb34
goldis <goldis.salarmaaf@hotmail.com>
2023-10-29 02:47:50 +0100

    deployment

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import obfuscator from 'rollup-plugin-obfuscator';
+const { resolve } = require("path");
 
 export default ({ command }) => {
   const production = command === 'build';
@@ -11,6 +12,11 @@ export default ({ command }) => {
       outDir: 'dist',
       minify: production,
       rollupOptions: {
+        input: {
+          index: resolve(__dirname, "./src/index.html"),
+          about: resolve(__dirname, "./src/pages/html/about.html"),
+          favorites: resolve(__dirname, "./src/pages/html/favorites.html"),
+        },
         output: {
           entryFileNames: 'assets/[name]-[hash].js',
           chunkFileNames: 'assets/[name]-[hash].js',
@@ -21,7 +27,7 @@ export default ({ command }) => {
     server: {
       port: 3000, // Port für den Entwicklungsserver
     },
-    base: '/FrontendJavascriptApplication/',
+    base: './',
     plugins: production ? plugins : [],
   });
 };
